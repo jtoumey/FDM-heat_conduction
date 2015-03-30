367fa30517e88bce2de94b783ee6bbd8e1dedfb7
Julian T <jtoumey@gmail.com>
2015-03-29 19:00:22 -0500

    added format statements and write loop to the write subroutine

--- conduction-1d/write_results.f90
+++ conduction-1d/write_results.f90
@@ -1,12 +1,21 @@
-subroutine write_results(T)
+subroutine write_results(n,x,T)
+!
+IMPLICIT NONE
+integer n,ii
+double precision x(n),T(n)
 
-
+open(unit=7,file='temp_distr.dat')
+!
+!   write header
+write(7,101)
+! loop over and write results
+do ii = 1,n
+   write(7,201)x(ii),T(ii)
+end do
 !
 !...Format statements
 !
-
-
-
-
-
+101 format(5x,'____x(n)____',5x,'____T(n)____')
+201 format(3x,f12.5,3x,f12.5)
+!
 end subroutine write_results
